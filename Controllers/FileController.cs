acc4f286a1e38e46256d28a01a5d5ecb26a37d95
Justin Buth <jb15512@appliedmed.com>
2022-05-18 16:26:48 -0700

    inject FileExtensionContentTypeProvider

---
+++ Controllers/FileController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+        public FileController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+        {
+            _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider 
+                ?? throw new System.ArgumentException(nameof(fileExtensionContentTypeProvider));
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Get(string id)
+        {
+            // lookup file
+            var path = "getting-started-with-rest-slides.pdf";
+
+            // check file exists
+            if (!System.IO.File.Exists(path))
+                return NotFound();
+
+            if(!_fileExtensionContentTypeProvider.TryGetContentType(path, out var contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+
+            var bytes = System.IO.File.ReadAllBytes(path);
+            return File(bytes, contentType, Path.GetFileName(Path.GetFileName(path)));
+
+        }
+    }
+}
