5e3f735cbc6842b7b7be7da08b2d6362be4aeaba
Justin Buth <jb15512@appliedmed.com>
2022-05-18 15:57:09 -0700

    Add project files.

---
+++ CitiesDataStore.cs
@@ -0,0 +1,81 @@
+﻿using CityInfo.API.Models;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "One w big park.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto
+                        {
+                            Id = 1, 
+                            Name = "Central Park",
+                            Description = "big park"
+                        },
+                        new PointOfInterestDto
+                        {
+                            Id = 2,
+                            Name = "Empire State",
+                            Description = "big bldg"
+                        },
+                    }
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Dallas",
+                    Description = "One w Mavericks.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto
+                        {
+                            Id = 3,
+                            Name = "American Airlines Center",
+                            Description = "stadium"
+                        },
+                        new PointOfInterestDto
+                        {
+                            Id = 4,
+                            Name = "Samurai Museum",
+                            Description = "museum"
+                        },
+                    }
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Oakland",
+                    Description = "One w warriors.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto
+                        {
+                            Id = 5,
+                            Name = "Oracel Arena",
+                            Description = "stadium"
+                        },
+                        new PointOfInterestDto
+                        {
+                            Id = 6,
+                            Name = "Golden Gate Bridge",
+                            Description = "red bridge"
+                        },
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ Controllers/PointOfInterestController.cs
@@ -0,0 +1,40 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    [ApiController]
+    public class PointOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestDto>> Get(int cityId)
+        {
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id.Equals(cityId));
+
+            if (city == null)
+                return NotFound();
+
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet]
+        [Route("{pointofinterestid}")]
+        public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int pointOfInterestId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id.Equals(cityId));
+
+            if (city == null)
+                return NotFound();
+
+            var poi = city.PointsOfInterest.FirstOrDefault(p => p.Id.Equals(pointOfInterestId));
+
+            if (poi == null)
+                return NotFound();
+
+            return Ok(poi);
+        }
+    }
+}
